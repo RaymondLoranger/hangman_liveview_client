cf5f441e16792bf4f67e73ebf236803b5b0cd370
RaymondLoranger <Raymond.Loranger@bell.net>
2022-03-16 18:52:46 -0400

    0.1.4

--- lib/hangman_liveview_client_web/views/hangman_view.ex
+++ lib/hangman_liveview_client_web/views/hangman_view.ex
@@ -5,6 +5,6 @@ defmodule Hangman.LiveView.ClientWeb.HangmanView do
 
   # rope, head, body, leg2, leg1, arm2, arm1
   @spec opacity(Game.turns_left(), non_neg_integer) :: String.t()
-  defp opacity(turns_left, gate) when turns_left > gate, do: "opacity-25"
-  defp opacity(_turns_left, _gate), do: "opacity-100"
+  defp opacity(turns_left, level) when turns_left > level, do: "opacity-25"
+  defp opacity(_turns_left, _level), do: "opacity-100"
 end
