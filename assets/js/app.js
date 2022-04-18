eecef13b7a8cd6a56bcbb5b51ff9194fcacf1784
RaymondLoranger <Raymond.Loranger@bell.net>
2022-04-18 14:48:35 -0400

    0.1.11

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+// We import the CSS which is extracted to its own file by esbuild.
+// Remove this line if you add a your own CSS build pipeline (e.g postcss).
+// import '../css/app.css'
+
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
 // to get started and then uncomment the line below.
 // import "./user_socket.js"
@@ -22,10 +26,13 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
-let csrfToken =
-  document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket =
-  new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content")
+
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken}
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
@@ -35,9 +42,8 @@ window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 // connect if there are any LiveViews on the page
 liveSocket.connect()
 
-// expose liveSocket on window for console debug logs and latency simulation:
+// expose liveSocket on window for web console debug logs & latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
--- config/config.exs
+++ config/config.exs
@@ -25,18 +25,23 @@ config :hangman_liveview_client, Hangman.LiveView.ClientWeb.Endpoint,
 config :esbuild,
   version: "0.14.0",
   default: [
-    args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets
-      --external:/fonts/* --external:/images/*),
+    args: ~w(
+      js/app.js --bundle --target=es2017 --outdir=../priv/static/assets
+      --external:/fonts/* --external:/images/*
+    ),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
 
 # Set Tailwind CSS version, path to config, and customize asset paths...
 config :tailwind,
-  version: "3.0.23",
+  version: "3.0.24",
   default: [
-    args: ~w(--config=tailwind.config.js --input=css/app.css
-      --output=../priv/static/assets/app.css),
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
     cd: Path.expand("../assets", __DIR__)
   ]
 
